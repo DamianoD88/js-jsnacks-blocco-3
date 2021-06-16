0ee68868cf1409469a308b1c7ea1d9f43c5f75fc
DamianoD88 <dispe88@live.it>
2021-06-16 16:54:45 +0200

    add prova 2 verso la fine

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // 1. Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
-var x = miaFunzione(1,7)
+// var x = miaFunzione(1,7)
 
-function miaFunzione(array, a, b) {
-    return a / b;
-}
+// function miaFunzione(array, a, b) {
+//     return a / b;
+// }
 
-var array = 0;
+// var array = 0;
 
 // Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.  Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
@@ -15,15 +15,17 @@ var word1 = prompt('inserisci la prima parola');
 var word2 = prompt('inserisci la seconda parola');
 
 function parole(a,b) {
-    var result = 0;
+    var result;
     if (a.length == b.length) {
         result = 'hanno la stessa lunghezza';
 
-    } else if (a.lenght < b-lenght) {
-        result = b;
-    } else (a.lenght > b.lenght) {
-        result = b;
+    } else if (a.length < b-length) {
+        result = 'b è più lunga';
+
+    } else if (a.length > b-length) {
+        result = 'a è più lunga';
     }
+        
     return result;
 }
 
