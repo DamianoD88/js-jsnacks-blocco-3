3d0b2f7b3d09994c69dfd399ab34d44b915f6bea
DamianoD88 <dispe88@live.it>
2021-06-17 15:09:42 +0200

    add fine terzo esercizio

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 // 1. Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
 
  
- var array = [5, 7, 12, 8, 15, 20, 50];
+//  var array = [5, 7, 12, 8, 15, 20, 50];
 
- var a = 1;
- var b = 5;
+//  var a = 1;
+//  var b = 5;
 
-//  var nuovoArray = [];
+// //  var nuovoArray = [];
 
-//  myfunction(array,a,b); 
+// //  myfunction(array,a,b); 
 
- console.log(myFunction, a, b);
+//  console.log(myFunction, a, b);
 
- function myFunction (array, min, max){
-     var nuovoArray = [];
-    for (var i = min - 1; i < max; i++){
-        if (a <= array.length && b >= array.length){
-            nuovoArray.push(array[i]);
-        }
-    }
-    return nuovoArray;
- }
+//  function myFunction (array, min, max){
+//      var nuovoArray = [];
+//     for (var i = min - 1; i < max; i++){
+//         if (a <= array.length && b >= array.length){
+//             nuovoArray.push(array[i]);
+//         }
+//     }
+//     return nuovoArray;
+//  }
 
 
 
@@ -51,4 +51,47 @@
 
 
 // 3.  Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro. Es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+ var arrayPrimo = ['a', 'b', 'c'];
+
+ var arraySecondo = [1, 2, 3];
+
+
+
+// var lengthList; 
+
+
+// console.log(arrayTerzo);
+
+
+
+ function myFunction(a, b) {
+    var arrayTerzo = [];
+
+    if (a.length == b.length) {
+        var lengthList = a.length;
+        for (var i = 0; i < lengthList; i++){
+            arrayTerzo.push(a[i]);
+            arrayTerzo.push(b[i]);
+        }
+    }else {
+        console.log('gli array non sono lunghi uguali'); 
+    }
+    return  arrayTerzo;
+ }
+
+//  myFunction(arrayPrimo, arraySecondo);
+ console.log(myFunction(arrayPrimo, arraySecondo));
+
+//  if (arrayPrimo.length == arraySecondo.length) {
+//     var lengthList = arrayPrimo.length;
+//     for (var i = 0; i < lengthList; i++){
+//         arrayTerzo.push(arrayPrimo[i]);
+//         arrayTerzo.push(arraySecondo[i]);
+//     }
+// }else {
+//     console.log('gli array non sono lunghi uguali'); 
+// }
+
+
 // 4. Scrivi una funzione che prenda una stringa e la trasformi con l'iniziale maiuscola e tutto il resto in minuscolo.
